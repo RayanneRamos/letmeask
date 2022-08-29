7f2a1ec5eae759d69cfb0c6494f4bdafd8bbee51
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-08-29 17:15:37 -0300

    Criando as rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AdminRoom } from './pages/AdminRoom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/rooms/new' element={<NewRoom />} />
+        <Route path='/rooms/:id' element={<Room />} />
+        <Route path='/admin/rooms/:id' element={<AdminRoom />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
