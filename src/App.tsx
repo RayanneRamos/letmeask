8a9e6c2eb53a04654fdec0c4d7612900d10aaecd
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-08-30 15:46:25 -0300

    Chamando o contexto no app

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import { AdminRoom } from './pages/AdminRoom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
+    <AuthContextProvider>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/rooms/new' element={<NewRoom />} />
-        <Route path='/rooms/:id' element={<Room />} />
-        <Route path='/admin/rooms/:id' element={<AdminRoom />} />
-      </Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms/new' element={<NewRoom />} />
+          <Route path='/rooms/:id' element={<Room />} />
+          <Route path='/admin/rooms/:id' element={<AdminRoom />} />
+        </Routes>
+    </AuthContextProvider>
     </BrowserRouter>
   );
 }
