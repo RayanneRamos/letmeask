369569bfdcf1c4901da908f8556371c4344a3f06
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-08-31 15:22:59 -0300

    Criando o ThemeContext para adicionar o dark mode ao projeto

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState, createContext, ReactNode } from 'react';
+
+type Theme = 'light' | 'dark';
+
+type ThemeContextProviderProps = {
+  children: ReactNode;
+}
+
+type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext({} as ThemeContextType);
+
+function ThemeContextProvider(props: ThemeContextProviderProps) {
+  const [ currentTheme, setCurrentTheme ] = useState<Theme>(() => {
+    const storagedTheme = localStorage.getItem('theme-letmeask');
+    
+    return (storagedTheme ?? 'light') as Theme;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('theme-letmeask', currentTheme);
+  }, [ currentTheme ]);
+
+  function toggleTheme() {
+    setCurrentTheme(currentTheme === 'light' ? 'dark' : 'light');
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+}
+
+export { ThemeContextProvider };
