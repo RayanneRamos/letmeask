315287bd599df17e0723960ffbcb83d62cd270a9
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-09-01 14:37:45 -0300

    Adicionando novos tipos de buttons no componente Button  e sua estilização

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,20 @@
 import { ButtonHTMLAttributes } from 'react';
+import cx from 'classnames';
+import { useTheme } from '../../hooks/useTheme';
 import './styles.scss';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   isOutlined?: boolean;
+  isLogout?: boolean;
+  isDanger?: boolean;
 }
 
-function Button({ isOutlined = false, ...props }: ButtonProps) {
+function Button({ isOutlined = false, isLogout = false, isDanger = false, ...props }: ButtonProps) {
+  const theme = useTheme();
+
   return (
     <button 
-      className={`button ${isOutlined ? 'outlined' : '' }`}
+      className={cx(`button ${theme}`, { outlined: isOutlined }, { logout: isLogout }, { danger: isDanger })}
       {...props}
     />
   );
