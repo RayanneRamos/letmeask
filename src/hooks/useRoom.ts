84d1d06470466a0944b199cf5abad7c89b7a8fcb
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-09-01 16:10:48 -0300

    Desabilitando o botão logout

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { database } from "../services/firebase";
 import { useAuth } from "./useAuth";
 
@@ -28,42 +29,57 @@ type FirebaseQuestions = Record<string, {
   }>
 }>
 
+type DataRoomProps = {
+  authorId: string;
+  endedAt: string;
+  title: string;
+}
+
 function useRoom(roomId: string) {
   const { user } = useAuth();
   const [ questions, setQuestions ] = useState<QuestionType[]>([]);
   const [ title, setTitle ] = useState('');
+  const [ dataRoom, setDataRoom ] = useState<DataRoomProps>();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const roomRef = database.ref(`rooms/${roomId}`);
 
     roomRef.on('value', room => {
       const databaseRoom = room.val();
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
 
-      const parsedQuestions = Object.entries(firebaseQuestions).map(([ key, value ]) => {
-        return {
-          id: key,
-          content: value.content,
-          author: value.author,
-          isHighLighted: value.isHighLighted,
-          isAnswered: value.isAnswered,
-          likeCount: Object.values(value.likes ?? {}).length,
-          likeId: Object.entries(value.likes ?? {}).find(([ key, like ]) => like.authorId === user?.id)?.[0],
-        }
-      });
+      if(databaseRoom !== null) {
+        const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+        setDataRoom(databaseRoom);
 
-      setTitle(databaseRoom.title);
-      setQuestions(parsedQuestions);
+        const parsedQuestions = Object.entries(firebaseQuestions).map(([ key, value ]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighLighted: value.isHighLighted,
+            isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(([ key, like ]) => like.authorId === user?.id)?.[0],
+          };
+        });
+        
+        setTitle(databaseRoom.title);
+        setQuestions(parsedQuestions);
+      } else {
+        navigate('/');
+      }
     });
 
     return () => {
       roomRef.off('value');
     }
-  }, [ roomId, user?.id ]);
+  }, [ roomId, user?.id, navigate ]);
 
   return {
     questions,
     title,
+    dataRoom,
   }
 }
 
