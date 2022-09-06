1e8dfe62f64d8c87d296b0f5f7509f306883fa82
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-09-06 15:10:43 -0300

    Criando a rota NotFound da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import { AdminRoom } from './pages/AdminRoom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
+import { NotFound } from './pages/NotFound';
 import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
-    <AuthContextProvider>
-      <Routes>
+      <AuthContextProvider>
+        <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/rooms/new' element={<NewRoom />} />
           <Route path='/rooms/:id' element={<Room />} />
           <Route path='/admin/rooms/:id' element={<AdminRoom />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
-    </AuthContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
