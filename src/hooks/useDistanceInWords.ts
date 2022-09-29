7b2731edf04591187662a169b44b6da243993b4d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-09-29 15:12:42 -0300

    Criando o createdAt com o moment

---
+++ src/hooks/useDistanceInWords.ts
@@ -0,0 +1,9 @@
+import moment from 'moment';
+
+function useDistanceInWords() {
+  return function (createdAt: number) {
+    return moment.duration(moment(new Date()).diff(createdAt)).humanize();
+  };
+}
+
+export { useDistanceInWords };
