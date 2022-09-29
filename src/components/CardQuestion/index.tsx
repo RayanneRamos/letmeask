4bcb8ec9ca28edcffdaea5a19a15fee6bca4d9ab
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-09-29 15:14:07 -0300

    Adicionando o createdAt nas perguntas

--- src/components/CardQuestion/index.tsx
+++ src/components/CardQuestion/index.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react';
 import cx from 'classnames';
 import { useTheme } from '../../hooks/useTheme';
 import './styles.scss';
+import { BadgeNewQuestion } from '../BadgeNewQuestion';
+import { useDistanceInWords } from '../../hooks/useDistanceInWords';
 
 type QuestionProps = {
   content: string;
@@ -9,14 +11,16 @@ type QuestionProps = {
     name: string;
     avatar: string;
   };
+  createdAt: number;
   children?: ReactNode;
   isAnswered?: boolean; 
   isHighLighted?: boolean;
   amountLike?: number;
 }
 
-function CardQuestion({ content, author, isAnswered = false, isHighLighted = false, amountLike, children }: QuestionProps) {
+function CardQuestion({ content, author, isAnswered = false, isHighLighted = false, amountLike, children, createdAt }: QuestionProps) {
   const { theme } = useTheme();
+  const distanceWords = useDistanceInWords();
 
   return (
     <div className={cx(`question ${theme}`, { answered: isAnswered }, { highlighted: isHighLighted && !isAnswered })}>
@@ -28,7 +32,9 @@ function CardQuestion({ content, author, isAnswered = false, isHighLighted = fal
         <div className='user-info'>
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
+          <span>Há {distanceWords(createdAt)}</span>
         </div>
+        <BadgeNewQuestion createdAt={createdAt} />
         <div>
           {children}
         </div>
