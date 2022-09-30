b3b8517e3dcaf2a606a2c972e6a6da2be22efad4
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-09-30 17:24:53 -0300

    Criação do hook useDistanceInWords

--- src/hooks/useDistanceInWords.ts
+++ src/hooks/useDistanceInWords.ts
@@ -3,7 +3,7 @@ import moment from 'moment';
 function useDistanceInWords() {
   return function (createdAt: number) {
     return moment.duration(moment(new Date()).diff(createdAt)).humanize();
-  };
+  }
 }
 
 export { useDistanceInWords };
