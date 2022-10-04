1b32229a10499ab16cb29d5a4ea2a2f2ec9a1d6a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-04 15:17:03 -0300

    Criação do componente Stats e a sua estilização

---
+++ src/components/Stats/index.tsx
@@ -0,0 +1,29 @@
+import './styles.scss';
+
+type QuestionsType = {
+  firstStats?: number | undefined;
+  secondStats?: number | undefined;
+  text: string;
+  borderColor?: string | undefined;
+  textColor?: string | undefined;
+  bgColor?: string | undefined;
+}
+
+function Stats({ firstStats = 0, secondStats = 0, text, borderColor, textColor = borderColor, bgColor = 'transparent' }: QuestionsType) {
+  return (
+    <div 
+      className='room-stats'
+      style={{ 
+        borderColor: `${borderColor}`,
+        color: `${textColor}`,
+        backgroundColor: `${bgColor}`,  
+      }}
+    >
+      <h3>
+        { firstStats ? firstStats : secondStats } { text }
+      </h3>
+    </div>
+  );
+}
+
+export { Stats };
