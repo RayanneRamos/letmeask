783a8a5ea3423e520fcc6e5ec00779aaf6f6c78a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-04 22:38:38 -0300

    Redirecionamento de encerramento de salas

---
+++ src/hooks/useStateRoom.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { database } from '../services/firebase';
+
+const useStateRoom = (roomId: string) => {
+  const [ state, setState ] = useState<boolean>(false);
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+
+      if(databaseRoom.endedAt) {
+        setState(true);
+      } else {
+        setState(false);
+      }
+    });
+
+    return () => {
+      roomRef.off('value');
+    };
+  }, [ [], roomId, state ]);
+
+  return state;
+}
+
+export { useStateRoom };
