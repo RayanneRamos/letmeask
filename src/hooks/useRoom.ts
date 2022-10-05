bd9b9c228c53fb3c06c9a62cc870093292a520fc
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-04 21:48:10 -0300

    Filtrando 20 perguntas no projeto

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -62,54 +62,57 @@ function useRoom(roomId: string) {
       }
     });
 
-    roomRef.on('value', room => {
-      const databaseRoom = room.val();
+    roomRef   
+      .child('questions')
+      .limitToFirst(20)
+      .on('value', room => {
+        const databaseRoom = room.val();
 
-      if(databaseRoom?.closedAt) {
-        showToast('🟥', 'Essa sala foi encerrada pelo administrador!!');
-        return navigate('/');
+        if(databaseRoom?.closedAt) {
+          showToast('🟥', 'Essa sala foi encerrada pelo administrador!!');
+          return navigate('/');
+        }
+
+        if(databaseRoom !== null) {
+          const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+          setDataRoom(databaseRoom);
+
+          const parsedQuestions = Object.entries(firebaseQuestions).map(([ key, value ]) => {
+            return {
+              id: key,
+              content: value.content,
+              author: value.author,
+              createdAt: value.createdAt,
+              isHighLighted: value.isHighLighted,
+              isAnswered: value.isAnswered,
+              likeCount: Object.values(value.likes ?? {}).length,
+              likeId: Object.entries(value.likes ?? {}).find(([ key, like ]) => like.authorId === user?.id)?.[0],
+            };
+          });
+
+          const orderQuestionsByLikeCount = parsedQuestions.sort((roomA, roomB) => (
+            roomA.likeCount < roomB.likeCount ? 1 : roomA.likeCount > roomB.likeCount ? -1 : 0
+          ));
+
+          const orderQuestionsByNotAnswer = orderQuestionsByLikeCount.sort((roomA, roomB) => (
+            roomA.isAnswered > roomB.isAnswered ? 1 : roomA.isAnswered < roomB.isAnswered ? -1 : 0
+          ));
+
+          setTitle(databaseRoom.title);
+          setQuestions(orderQuestionsByNotAnswer);
+          setCreatedAt(databaseRoom.createdAt);
+          setEndedAt(databaseRoom.endedAt);
+          setCheckIsAdmin(dataRoom?.authorId === user?.id ? true : false);
+          setAvatar(databaseRoom?.avatar);
+          setName(databaseRoom?.name);
+        } else {
+          navigate('/');
+        }
+      });
+
+      return () => {
+        roomRef.off('value');
       }
-
-      if(databaseRoom !== null) {
-        const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-        setDataRoom(databaseRoom);
-
-        const parsedQuestions = Object.entries(firebaseQuestions).map(([ key, value ]) => {
-          return {
-            id: key,
-            content: value.content,
-            author: value.author,
-            createdAt: value.createdAt,
-            isHighLighted: value.isHighLighted,
-            isAnswered: value.isAnswered,
-            likeCount: Object.values(value.likes ?? {}).length,
-            likeId: Object.entries(value.likes ?? {}).find(([ key, like ]) => like.authorId === user?.id)?.[0],
-          };
-        });
-
-        const orderQuestionsByLikeCount = parsedQuestions.sort((roomA, roomB) => (
-          roomA.likeCount < roomB.likeCount ? 1 : roomA.likeCount > roomB.likeCount ? -1 : 0
-        ));
-
-        const orderQuestionsByNotAnswer = orderQuestionsByLikeCount.sort((roomA, roomB) => (
-          roomA.isAnswered > roomB.isAnswered ? 1 : roomA.isAnswered < roomB.isAnswered ? -1 : 0
-        ));
-
-        setTitle(databaseRoom.title);
-        setQuestions(orderQuestionsByNotAnswer);
-        setCreatedAt(databaseRoom.createdAt);
-        setEndedAt(databaseRoom.endedAt);
-        setCheckIsAdmin(dataRoom?.authorId === user?.id ? true : false);
-        setAvatar(databaseRoom?.avatar);
-        setName(databaseRoom?.name);
-      } else {
-        navigate('/');
-      }
-    });
-
-    return () => {
-      roomRef.off('value');
-    }
   }, [roomId, checkIsAdmin, user?.id, navigate, showToast, dataRoom?.authorId]);
 
   return {
