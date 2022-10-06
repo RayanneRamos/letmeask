9eb0a3e1cfbe3e6ea3854a1ea51d817b622170ec
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-05 22:17:07 -0300

    Adicionando as animações na página Room

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -18,6 +18,8 @@ import { Stats } from '../../components/Stats';
 import { useStateRoom } from '../../hooks/useStateRoom';
 import { FiCornerDownRight } from 'react-icons/fi';
 import { Answer } from '../../components/Answer';
+import { motion } from 'framer-motion';
+import { fadeInUp, stagger } from '../../styles/animation';
 
 type RoomParams = {
   id: string;
@@ -147,7 +149,14 @@ function Room() {
   }
 
   return (
-    <div id='page-room' className={theme}>
+    <motion.div 
+      id='page-room' 
+      className={theme}
+      variants={stagger}
+      initial='initial'
+      animate='animate'
+      exit={{ opacity: 0 }}
+    >
       <header>
         <div className='content'>
           <Link to='/'>
@@ -160,7 +169,7 @@ function Room() {
         </div>
         <Toaster toastOptions={{ duration: 2100 }} />
       </header>
-      <main>
+      <motion.main variants={fadeInUp}>
         <div className='room-title'>
           <div className='avatar'>
             <img src={user?.avatar} alt={user?.name} />
@@ -225,36 +234,44 @@ function Room() {
                     createdAt={question.createdAt}
                   >
                     { !question.isAnswered && (
-                      <button
+                      <motion.button
                         className={`like-button ${question.likeId ? 'liked' : ''}`}
                         type='button'
                         aria-label='Marcar como gostei'
                         onClick={() => handleLikeQuestion(question.id, question.likeId)}
+                        whileTap={
+                          !question.likeId && user ? {
+                            scale: 1.1,
+                          } : {}
+                        }
                       >
                         { question.likeCount > 0 && <span>{question.likeCount}</span> }
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
-                      </button>
+                      </motion.button>
                     ) }
                   </CardQuestion>
                 </div>
                 { question.answers.map((v) => (
-                  <div
+                  <motion.div
                     className='answer-container'
                     key={v.content}
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                    transition={{ duration: 0.4 }}
                   >
                     <FiCornerDownRight size={24} color='#e559f9' />
                     <Answer author={v.author} content={v.content} />
-                  </div>
+                  </motion.div>
                 )) }
               </>
             )) }
            </div>
           )}
         </div>
-      </main>
-    </div>
+      </motion.main>
+    </motion.div>
   );
 }
 
