a8b04246d6d0ff34e2d4aa7fd0507572e1532330
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-05 22:23:13 -0300

    Adicionando as animações na página NotFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -4,21 +4,30 @@ import logoDarkImage from '../../assets/images/logo-dark.svg';
 import { Button } from '../../components/Button';
 import { useTheme } from '../../hooks/useTheme';
 import './styles.scss';
+import { motion } from 'framer-motion';
+import { fadeInUp, stagger } from '../../styles/animation';
 
 function NotFound() {
   const navigate = useNavigate();
   const { theme } = useTheme();
 
   return(
-    <div id='page-notfound' className={theme}>
-      <main>
+    <motion.div 
+      id='page-notfound' 
+      className={theme}
+      variants={stagger}
+      initial='initial'
+      animate='animate'
+      exit={{ opacity: 0 }}
+    >
+      <motion.main variants={fadeInUp} >
         <div className='container'>
           <img src={theme === 'light' ? logoImage : logoDarkImage} alt='Letmeask' />
           <h2>Error 404: Page not found</h2>
           <Button type='submit' onClick={() => navigate('/')}>Voltar</Button>
         </div>
-      </main>
-    </div>
+      </motion.main>
+    </motion.div>
   );
 }
 
